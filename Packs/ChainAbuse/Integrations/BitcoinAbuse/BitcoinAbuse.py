2fa2bb2b213e473b465f8a011800b622f960495a
Leonardo Godoy <87772464+lgodoys@users.noreply.github.com>
2025-04-01 23:23:22 +0000

    Created "ChainAbuse" integration to replace "Bitcoin Abuse"

---
+++ Packs/ChainAbuse/Integrations/BitcoinAbuse/BitcoinAbuse.py
@@ -0,0 +1,211 @@
+import demistomock as demisto  # noqa: F401
+from CSVFeedApiModule import *
+from CommonServerPython import *  # noqa: F401
+
+# disable insecure warnings
+urllib3.disable_warnings()
+
+""" CONSTANTS """
+SERVER_URL = "https://api.chainabuse.com/v0/"
+
+
+class ChainAbuseClient(BaseClient):
+    def __init__(self, base_url, insecure, proxy, api_key):
+        super().__init__(base_url=base_url, verify=not insecure, proxy=proxy)
+        self.server_url = base_url
+        self.api_key = api_key
+        self.insecure = insecure
+
+    def report_address(self, args: Dict) -> Dict:
+        """
+        Sends a post request to report an abuse to ChainAbuse servers.
+        """
+        
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authentication": f"Basic {base64.b64encode(f'{self.api_key}:{self.api_key}'.encode()).decode()}"
+        }
+
+        body = [
+            {
+                "accusedScammers": [{}],
+                "addresses": [{}],
+                "tokens": [{}],
+                "transactionHashes": [{}],
+                "agreedToBeContactedData": {},
+                "categoryDescription": None,
+                "compromiseIndicators": [{}],
+                "description": "",
+                "evidences": [{}],
+                "losses": [{}],
+                "scamCategory": None,
+            }
+        ]
+
+        for k, v in args:
+            if "cntc" in k:
+                if k == "cntc" and v or v == "true":
+                    body["agreedToBeContactedData"]["agreed"] = v  # type: ignore
+                    if k == "cntcLaw":
+                        body["agreedToBeContactedData"]["agreedToBeContactedByLawEnforcement"] = v  # type: ignore
+                    elif k == "cntcName":
+                        body["agreedToBeContactedData"]["name"] = v  # type: ignore
+                    elif k == "cntcEmail":
+                        body["agreedToBeContactedData"]["email"] = v  # type: ignore
+                elif k == "cntc" and not v or v == "false":
+                    body["agreedToBeContactedData"]["agreed"] = v  # type: ignore
+            elif "add" in k:
+                if k == "add_address":
+                    body["addresses"][0]["address"] = v  # type: ignore
+                elif k == "add_domain":
+                    body["addresses"][0]["domain"] = v  # type: ignore
+                elif k == "add_chain":
+                    body["addresses"][0]["chain"] = v  # type: ignore
+                elif k == "add_label":
+                    body["addresses"][0]["label"] = v  # type: ignore
+            elif "accused" in k:
+                if k == "accusedContact":
+                    body["accusedScammers"][0]["contact"] = v  # type: ignore
+                elif k == "accusedType":
+                    body["accusedScammers"][0]["type"] = v  # type: ignore
+            elif "tokens" in k:
+                body["tokens"][0]["tokenId"] = v  # type: ignore
+            elif "transact" in k:
+                if k == "transactHash":
+                    body["transactionHashes"][0]["hash"] = v  # type: ignore
+                elif k == "transactChain":
+                    body["transactionHashes"][0]["chain"] = v  # type: ignore
+                elif k == "transactLabel":
+                    body["transactionHashes"][0]["label"] = v  # type: ignore
+            elif "comp" in k:
+                if k == "compValue":
+                    body["compromiseIndicators"][0]["value"] = v  # type: ignore
+                elif k == "compType":
+                    body["compromiseIndicators"][0]["type"] = v  # type: ignore
+            elif "evidence" in k:
+                body["evidences"][0]["source"] = v  # type: ignore
+            elif "loss" in k:
+                if k == "lossAsset":
+                    body["losses"][0]["asset"] = v  # type: ignore
+                elif k == "lossAmount":
+                    body["losses"][0]["amount"] = v  # type: ignore
+            else:
+                body[k] = v  # type: ignore
+
+        return self._http_request(method="POST",url_suffix="reports/batch",json_data=body,headers=headers)
+
+    def get_report(self, args) -> Tuple[List[Dict], bool]:
+        """
+        Builds CSV module client and performs the API call to Chain Abuse service.
+        If the call was successful, returns list of indicators.
+        Args:
+
+        Returns:
+            - Throws exception if an invalid api key was given or error occurred during the call to
+              Chain Abuse service.
+            - Returns indicators list if the call to Chain Abuse service was successful.
+        """
+
+        return self._http_request(
+            method="GET",
+            url_suffix="reports",
+            params=args,
+        )
+
+
+""" COMMAND FUNCTIONS """
+
+
+def chainabuse_report_command(cCoin_client: ChainAbuseClient, args: Dict) -> CommandResults:
+    """|
+    Reports a bitcoin abuse to Chain Abuse service.
+
+    Args:
+        cCoin_client (ChainAbuseClient): Client object to perform request.
+        args (Dict): Demisto args for report address command.
+
+    Returns:
+        str: 'bitcoin address (address reported) by abuser (abuser reported) was
+        reported to ChainAbuse API' if http request was successful'.
+    """
+    address = args.get("add_address")
+    domain = args.get("add_domain")
+    abuser = args.get("accusedContact")
+
+    http_response = cCoin_client.report_address(args)
+
+    if argToBoolean(http_response.get("success", False)):
+        return CommandResults(
+            readable_output=f"Chain address {address} or domain {domain} by abuse bitcoin user {abuser}"
+            f" was reported to ChainAbuse service"
+        )
+    else:
+        failure_message = http_response.get("response", "Unknown failure reason")
+        raise DemistoException(f"Chain report address did not succeed: {failure_message}")
+
+
+def test_module_command(cCoin_client: ChainAbuseClient):
+    """
+    Performs a fetch indicators flow to validate the configuration params.
+
+    Args:
+        cCoin_client (ChainAbuseClient): Client object.
+
+    Returns:
+        'ok' if the call to Chain Abuse service was successful and command is test_module.
+    """
+    cCoin_client.get_report(args={})
+    return "ok"
+
+
+def chainabuse_get_report(cCoin_client: ChainAbuseClient, args: Dict):
+    """
+    Wrapper for retrieving indicators from the feed to the war-room.
+
+    Args:
+        cCoin_client (ChainAbuseClient): Client object.
+        args (Dict): Demisto args.
+
+    Returns:
+        CommandResults.
+    """
+    indicators, _ = cCoin_client.get_report(args)
+    limit = arg_to_number(args.get("limit", 50), "limit")
+    truncated_indicators_list = indicators[:limit]
+    return CommandResults(
+        readable_output=tableToMarkdown("Indicators", truncated_indicators_list, headers=["value", "type", "fields"]),
+        raw_response=truncated_indicators_list,
+    )
+
+
+def main() -> None:
+    params = demisto.params()
+    command = demisto.command()
+
+    demisto.debug(f"Chain Abuse: Command being called is {demisto.command()}")
+
+    api_key = params.get("credentials_api_key", {}).get("password") or params.get("api_key", "")
+    if not api_key:
+        raise DemistoException("API Key must be provided.")
+    insecure = params.get("insecure", False)
+    proxy = params.get("proxy", False)
+
+    try:
+        cCoin_client = ChainAbuseClient(base_url=SERVER_URL, insecure=insecure, proxy=proxy, api_key=api_key)
+
+        if command == "test-module":
+            return_results(test_module_command(cCoin_client))
+        elif command == "chainabuse_report_command":
+            return_results(chainabuse_report_command(cCoin_client, demisto.args()))
+
+        elif command == 'chainabuse-get-report':
+            return_results(chainabuse_get_report(cCoin_client, demisto.args()))
+
+    # Log exceptions and return errors
+    except Exception as e:
+        return_error(f"Failed to execute {demisto.command()} command.\nError:\n{str(e)}")
+
+
+if __name__ in ("__main__", "__builtin__", "builtins"):
+    main()
---
+++ Packs/ChainAbuse/Integrations/BitcoinAbuse/README.md
@@ -0,0 +1,100 @@
+BitcoinAbuse.com is a public database of bitcoin addresses used by hackers and criminals.
+Supported Cortex XSOAR versions: 5.5.0 and later.
+
+## Get Your API Key
+
+In order to use Bitcoin Abuse service, you need to get your API key.
+The API key is free and can be achieved by doing the following:
+
+1. Navigate to <https://www.chainabuse.com> and click on "Login" on top right corner of your screen.
+2. If you have an account, login. If you have not an account, click on "Don't have an account? Sign up".
+2. Fill in your details (Email, Password) as required. Read and accept Privacy Policy.
+3. After your account have been set, and you have verified your account, go to Settings, and click on "API" section.
+4. Copy and save this API key in a safe place. This API key will be permanently hide in case you need to recover it.
+
+## Configure BitcoinAbuse in Cortex
+
+
+| **Parameter** | **Description** | **Required** |
+| --- | --- | --- |
+| api_key | API Key | True |
+| insecure | Trust any certificate \(not secure\) | False |
+| proxy | Use system proxy settings | False |
+
+
+## Commands
+
+You can execute these commands from the CLI, as part of an automation, or in a playbook.
+After you successfully execute a command, a DBot message appears in the War Room with the command details.
+
+### chainabuse-report
+
+***
+Reports an abuse address and/or domain to Chain Abuse service.
+
+#### Base Command
+
+`chainabuse-report`
+
+#### Input
+
+| **Argument Name** | **Description** | **Required** |
+| --- | --- | --- |
+| address | Address of the abuser. | Required | 
+| abuser | Information about the abuser. | Required | 
+| description | Description of the abuse. | Optional | 
+| abuse_type | Type of abuse. The "abuse_type_other" field is required when the value of the "abuse_type" field is "other". Possible values are "ransomware", "darknet market", "bitcoin tumber", "blackmail scam", "sextortion", and "other". Possible values are: ransomware, darknet market, bitcoin tumbler, blackmail scam, sextortion, other. | Required | 
+| abuse_type_other | Description of the abuse type. The "abuse_type_other" field is required when the value of the "abuse_type" field is "other". | Optional | 
+
+
+#### Context Output
+
+There is no context output for this command.
+
+#### Command Example
+
+```!bitcoinabuse-report-address address=abcde12345 abuser=abuser@abuse.net abuse_type="bitcoin tumbler" description="this is a description of the abuse"```
+
+
+#### Human Readable Output
+
+>Bitcoin address abcde12345 by abuse bitcoin user abuser@abuse.net was reported to BitcoinAbuse API
+
+### bitcoinabuse-get-indicators
+
+***
+Gets indicators from the feed.
+
+
+#### Base Command
+
+`bitcoinabuse-get-indicators`
+
+#### Input
+
+| **Argument Name** | **Description** | **Required** |
+| --- | --- | --- |
+| limit | The maximum number of results to return. Default is 50. | Optional | 
+
+
+#### Context Output
+
+There is no context output for this command.
+
+#### Command Example
+
+```!bitcoinabuse-get-indicators limit=1```
+
+#### Context Example
+
+```json
+{}
+```
+
+#### Human Readable Output
+
+>### Indicators
+
+>|value|type|fields|
+>|---|---|---|
+>| bitcoin:1MfhfDZdv2QXmBBZMom5ZnZzp8VVrJUENw | Cryptocurrency Address | Value: bitcoin:1MfhfDZdv2QXmBBZMom5ZnZzp8VVrJUENw<br/>rawaddress: 1MfhfDZdv2QXmBBZMom5ZnZzp8VVrJUENw<br/>countryname: Australia<br/>creationdate: 2021-01-17T00:30:36.000000Z<br/>description: I know ******** is one of your password on day of hack..<br/><br/>Lets get directly to the point.<br/>Not one person has paid me to check about you.<br/><br/>You do not know me and you're probably thinking why you are getting this email?<br/>in fact, i actually placed a malware on the adult vids (adult porn) website and you know what, you visited this site to experience fun (you know what i mean).<br/>When you were viewing videos, your browser started out operating as a RDP having a key logger which provided me with accessibility to your display and web cam.<br/><br/><br/>immediately after that, my malware obtained every one of your contacts from your Messenger, FB, as well as email account.<br/><br/><br/>after that i created a double-screen video. 1st part shows the video you were viewing (you have a nice taste omg), and 2nd part displays the recording of your cam, and its you.<br/>Best solution would be to pay me $2763.<br/><br/><br/>We are going to refer to it as a donation. in this situation, i most certainly will without delay remove your video.<br/><br/><br/><br/>Bitcoin address: 1MfhfDZdv2QXmBBZMom5ZnZzp8VVrJUENw<br/><br/>[case SeNSiTiVe, copy & paste it]<br/><br/><br/>You could go on your life like this never happened and you will not ever hear back again from me.<br/><br/><br/>You'll make the payment via Bitcoin (if you do not know this, search 'how to buy bitcoin' in Google).<br/>if you are planning on going to the law, surely, this e-mail can not be traced back to me, because it's hacked too.<br/>I have taken care of my actions. i am not looking to ask you for a lot, i simply want to be paid.<br/>if i do not receive the bitcoin;, i definitely will send out your video recording to all of your contacts including friends and family, co-workers, and so on.<br/>Nevertheless, if i do get paid, i will destroy the recording immediately.<br/>If you need proof, reply with Yeah then i will send out your video recording to your 8 friends.<br/>it's a nonnegotiable offer and thus please don't waste mine time & yours by replying to this message.<br/>abusetype: ransomware<br/>tags: <br/>reportscount: 1<br/>cryptocurrencyaddresstype: bitcoin |
